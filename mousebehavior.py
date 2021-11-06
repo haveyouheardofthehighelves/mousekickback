1db136d1a86d3b6cae89594a525495ce5c8d12ef
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2021-11-06 16:10:09 -0700

    Update mousebehavior.py

--- mousebehavior.py
+++ mousebehavior.py
@@ -1,17 +1,40 @@
 from pynput.mouse import Listener
 import logging
+import serial
+
+logging.basicConfig(filename="mouse_log.txt", level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+holding = False
+ser = serial.Serial("COM6", 9600, timeout=0)
+ser.xonxoff = 1
+
+def on_move(x, y):
+    file1 = open("weapon.txt", "r")  # write mode
+    print(file1.read())
+    file1.close()
 
 def readin(x, y):
+    global holding
     file1 = open("weapon.txt", "r")
-    print(file1.read())
+    if str(file1.readline()).strip() != "":
+        holding = True
+    else:
+        holding = False
     file1.close()
 
 
 def firing(x, y, button, pressed):
-    if pressed:
-        print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
-    else:
-        print('released ' + str(button))
+    global holding
+    if holding:
+        if str(button) == "Button.left":
+            if pressed:
+                ser.write(b'e')
+                ser.write(b'1')
+                ser.write(b'e')
+            else:
+                ser.write(b'e')
+                ser.write(b'0')
+                ser.write(b'e')
 
 
 with Listener(on_move=readin, on_click=firing) as listener:
