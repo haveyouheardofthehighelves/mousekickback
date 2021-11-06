bb631d11dc6093b7b3b4743b99c2bd9cf50f1a52
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2021-11-06 09:06:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+import pytesseract as tess
+import pyautogui
+from PIL import Image
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# list of primary weapons smg,rifles, machine guns,
+primary_weapons_list = [["stinger", "spectre"],
+                        ["bulldog", "guardian", "phantom", "vandal"],
+                        ["marshal", "operator"],
+                        ["ares", "odin"],
+                        ["bucky", "judge"]]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def stuffs():
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(r'C:\Users\sethy\PycharmProjects\Valorantshooting\valorant.png')
+    img = Image.open('valorant.png')
+    width, height = img.size
+    left = 1700
+    top = height / 1.3
+    right = 1850
+    bottom = 925
+    primary_weapon = img.crop((left, top, right, bottom))
+    data = [tess.image_to_string(primary_weapon, lang='eng', config='--psm 6')]
+    print(data[0])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("hi")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+stuffs()
