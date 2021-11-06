6e17e460117e2b6bf8f33b00b03743ca043759d1
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2021-11-06 10:22:48 -0700

    Added psm chart

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ from PIL import Image
 import time
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Page segmentation modes:
+#   0    Orientation and script detection (OSD) only.
+#   1    Automatic page segmentation with OSD.
+#   2    Automatic page segmentation, but no OSD, or OCR.
+#   3    Fully automatic page segmentation, but no OSD. (Default)
+#   4    Assume a single column of text of variable sizes.
+#   5    Assume a single uniform block of vertically aligned text.
+#   6    Assume a single uniform block of text.
+#   7    Treat the image as a single text line.
+#   8    Treat the image as a single word.
+#   9    Treat the image as a single word in a circle.
+#  10    Treat the image as a single character.
+#  10    Treat the image as a single character.
+#  11    Sparse text. Find as much text as possible in no particular order.
+#  12    Sparse text with OSD.
+#  13    Raw line. Treat the image as a single text line,
+#                         bypassing hacks that are Tesseract-specific.
 # list of primary weapons smg,rifles, machine guns,
 primary_weapons_list = [["stinger", "spectre"],
                         ["bulldog", "guardian", "phantom", "vandal"],
