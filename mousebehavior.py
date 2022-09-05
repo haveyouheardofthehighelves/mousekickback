b498f920fbbd983c58e23234cfc74eb9e9a1217b
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2022-09-05 16:37:41 -0700

    Update mousebehavior.py

--- mousebehavior.py
+++ mousebehavior.py
@@ -5,9 +5,10 @@ import serial
 logging.basicConfig(filename="mouse_log.txt", level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 holding = False
-ser = serial.Serial("COM6", 9600, timeout=0)
+ser = serial.Serial("COM7", 9600, timeout=0)
 ser.xonxoff = 1
-
+save = ""
+pressing = False
 def on_move(x, y):
     file1 = open("weapon.txt", "r")  # write mode
     print(file1.read())
@@ -15,26 +16,58 @@ def on_move(x, y):
 
 def readin(x, y):
     global holding
+    global save
+    #a = ser.readline()
     file1 = open("weapon.txt", "r")
-    if str(file1.readline()).strip() != "":
+    a = str(file1.read()).strip()
+    if a != "":
         holding = True
     else:
         holding = False
+    if holding and not pressing and save != a:
+        ser.write(b'w')
+        if a == "vandal":
+            ser.write(b'3')
+            ser.write(b'6')
+        elif a == "phantom":
+            ser.write(b'2')
+            ser.write(b'5')
+        elif a == "spectre":
+            ser.write(b'2')
+            ser.write(b'0')
+        elif a == "stinger":
+            ser.write(b'1')
+            ser.write(b'5')
+        elif a == "ares":
+            ser.write(b'3')
+            ser.write(b'5')
+        elif a == "odin":
+            ser.write(b'2')
+            ser.write(b'5')
+        elif a == "bulldog":
+            ser.write(b'4')
+            ser.write(b'5')
+        elif a == "bucky" or "judge" or "operator" or "marshall" or "guardian":
+            ser.write(b't')
+        ser.write(b'w')
+        save = a
+
     file1.close()
-
-
 def firing(x, y, button, pressed):
     global holding
+    global pressing
     if holding:
         if str(button) == "Button.left":
             if pressed:
                 ser.write(b'e')
                 ser.write(b'1')
                 ser.write(b'e')
+                pressing = True
             else:
                 ser.write(b'e')
                 ser.write(b'0')
                 ser.write(b'e')
+                pressing = False
 
 
 with Listener(on_move=readin, on_click=firing) as listener:
